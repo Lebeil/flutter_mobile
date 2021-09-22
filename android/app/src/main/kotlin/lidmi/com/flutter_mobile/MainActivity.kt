0c13af43b2bf4167c35c5d6216a7263d713df11d
Lebeil <liedel75@gmail.com>
2021-09-22 13:39:35 +0200

    first commit

---
+++ android/app/src/main/kotlin/lidmi/com/flutter_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package lidmi.com.flutter_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Les widgets',
+      theme: ThemeData(
+        primarySwatch: Colors.red
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _Home();
+  }
+}
+
+class _Home extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text('Salut'),
+          leading: const Icon(Icons.account_circle),
+          actions: const <Widget>[
+            Icon(Icons.access_alarm),
+            Icon(Icons.golf_course),
+            Icon(Icons.directions_bike),
+          ],
+          elevation: 10.0,
+          centerTitle: true,
+      ),
+      body: Container(
+        color: Colors.blue,
+        margin: const EdgeInsets.only(top: 20.0, bottom: 15.0),
+        child: const Center(
+          child: Card(
+            elevation: 5.0,
+            color: Colors.teal,
+            child: SizedBox(
+              width: 300.0,
+              height: 250.0,
+            )
+          )
+        )
+      )
+    );
+  }
+
+}
